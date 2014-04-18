78949b7cf2fa9a828235326277c2450d16d8fbdc
ZeroUltimax <antoinerobertson92@gmail.com>
2014-04-18 18:50:46 -0400

    Added node related methods to World class
    -Clone which node the robot is on
    -getting which node the robot is on
    -Adding node (incomplete)

--- Robots/src/World.java
+++ Robots/src/World.java
@@ -6,13 +6,18 @@ public class World {
 	
 	private ArrayList<Node> nodes;
 
-	private TreeMap<Robot,Node> robotPositions;
+	private TreeMap<Robot,Integer> robotNodeId; // Id of the nodes for each robot
+	
+	public  World(){
+		throw new UnsupportedOperationException();
+	}
 	
 	public Node getNode(int id) {
 		return nodes.get(id);
 	}
 	
 	public World duplicate(){
+
 		World result = new World();
 		
 		// Start by creating empty nodes
@@ -27,7 +32,23 @@ public class World {
 			}
 		}
 		
+		
+		// Copy the mapping for the robot
+		result.robotNodeId = new TreeMap<Robot,Integer>(this.robotNodeId);
+		
 		return result;
 		
 	} 
+
+	public Node getNode(Robot r){
+		return nodes.get(robotNodeId.get(r));
+	}
+
+	public void addNode(int nodeDegree, Node parentNode) {
+		throw new UnsupportedOperationException();
+	}
+	
+	private int nextNodeId(){
+		return nodes.size();
+	}
 }
