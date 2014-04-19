0cd745a71c159bde55c00758f60439d73dec56c8
ZeroUltimax <antoinerobertson92@gmail.com>
2014-04-19 15:39:18 -0400

    Implemented functions to world
    - Add a new node
    - Get a list of open nodes with a certain degree
    - Very useful for expanding hypotheses
    - Move robot from it's position to a neighboring node

--- Robots/src/World.java
+++ Robots/src/World.java
@@ -21,12 +21,12 @@ public class World {
 		World result = new World();
 		
 		// Start by creating empty nodes
-		for(Node n:nodes){
+		for(Node n:this.nodes){
 			result.nodes.add(new Node(n.getId(),n.getDegree()));
 		}
 		
 		// Clone the edges into the result world
-		for(Node n:nodes){
+		for(Node n:this.nodes){
 			for(Edge e:n.getEdges()){
 				e.cloneToWorld(result); // The new edge binds automatically
 			}
@@ -44,11 +44,35 @@ public class World {
 		return nodes.get(robotNodeId.get(r));
 	}
 
-	public void addNode(int nodeDegree, Node parentNode) {
-		throw new UnsupportedOperationException();
+	public Node addNode(int nodeDegree, Node parentNode) {
+		Node newNode =  new Node(nextNodeId(),nodeDegree);
+		parentNode.setParentOf(newNode);
+		nodes.add(newNode);
+		return newNode;
 	}
 	
 	private int nextNodeId(){
 		return nodes.size();
+	}
+	
+	public ArrayList<Node> getOpenNodes(int nodeDegree,Node excludeNode){		
+		ArrayList<Node> result = new ArrayList<Node>();
+		for(Node n:nodes){
+			if(n.getDegree()==nodeDegree&&n!=excludeNode){
+				result.add(n);
+			}
+		}
+		return result;
+	}
+
+	public void moveRobot(Robot movingRobot, Node endPoint) {
+		Node startPoint = this.getNode(movingRobot);
+		for(Edge e:startPoint .getEdges()){
+			if(e.traverse(startPoint)==endPoint){
+				robotNodeId.put(movingRobot, nodes.indexOf(endPoint));
+				return;
+			}
+		}
+		throw new BadEdgeException("No existing edge to endPoint from current robot position");
 	}
 }
