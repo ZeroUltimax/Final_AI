08e5aa9bca39e67cf737541937bff4604c4c6fb3
ZeroUltimax <antoinerobertson92@gmail.com>
2014-04-19 15:41:13 -0400

    Added node junction methods
    Methods to create an edge from one node to another
    - Cross to: create crossing edge to node
    - Set parent off: Create main edge to node

--- Robots/src/Node.java
+++ Robots/src/Node.java
@@ -1,4 +1,3 @@
-
 public class Node {
 
 	private int Id;
@@ -26,12 +25,48 @@ public class Node {
 		return edges;
 	}
 
-	
-	public Node getParent(){
-		if(this.Id==0){
+	public Node getParent() {
+		if (this.Id == 0) {
 			return null; // Root of the graph
-		}else{
+		} else {
 			return edges[0].traverse(this); // Traverse edge 0 to go to parent
 		}
 	}
+
+	private int getOpenEdgeId() {
+		for (int i = 0; i < this.edges.length; i++) {
+			if (edges[i].isOpen()) {
+				return i;
+			}
+		}
+		throw new BadEdgeException("No Open edge found");
+	}
+
+	public boolean hasOpen() {
+		for (Edge e : edges) {
+			if (e.isOpen()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void joinToNode(Node otherNode, int otherOpenId, boolean isCross) {
+		int thisOpenId = this.getOpenEdgeId();
+
+		Edge fusedEdge = Edge.fuse(this.edges[thisOpenId],
+				otherNode.edges[otherOpenId], isCross);
+
+		this.edges[thisOpenId] = fusedEdge;
+		otherNode.edges[otherOpenId] = fusedEdge;
+	}
+
+	public void crossTo(Node otherNode) {
+		joinToNode(otherNode, otherNode.getOpenEdgeId(), true);
+	}
+
+	public void setParentOf(Node newNode) {
+		joinToNode(newNode, 0, false);
+	}
+
 }
